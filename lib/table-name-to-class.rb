700e28d37d3cbf0bac086cb8e9574142e6da179e
Morgen Peschke <morgen.peschke@calltrackingmetrics.com>
2014-04-21 11:11:01 -0400

    Initial Commit

---
+++ lib/table-name-to-class.rb
@@ -0,0 +1,34 @@
+class TableNameToClass
+  @@conversion_hash = nil
+
+  def self.convert(name, force_update = nil)
+    begin
+      name.classify.constantize
+    rescue NameError
+      init_hash unless @@conversion_hash || force_update
+      @@conversion_hash[name]
+    end
+  end
+
+  def self.debug
+    return @@conversion_hash
+  end
+
+  private
+
+  def self.init_hash
+    # Expanded from idea posted here: http://stackoverflow.com/a/6150228
+
+    # Recursively require all models
+    Dir.glob(Rails.root.join('app','models','**','*').to_s).each do |f|
+      begin
+        require f if File.file?(f) && f =~ /\.rb$/
+      rescue IdentityCache::AlreadyIncludedError
+      end
+    end
+
+    @@conversion_hash = Hash[
+      ActiveRecord::Base.descendants.map {|c| [c.table_name, c]}
+    ]
+  end
+end
---
+++ table-name-to-class.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'table-name-to-class'
+  s.version     = '0.0.1'
+  s.date        = '2014-04-21'
+  s.summary     = 'Converts database table names to class constants'
+  s.description = 'A slightly smarter conversion than .classify'
+  s.authors     = ['Morgen Peschke']
+  s.email       = 'morgen.peschke@gmail.com'
+  s.files       = ['lib/table-name-to-class.rb']
+  s.homepage    = 'https://github.com/morgen-peschke/table-name-to-class'
+  s.license     =  'MIT'
+end
