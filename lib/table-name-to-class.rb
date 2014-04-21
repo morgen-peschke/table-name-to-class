9a66e9eb9c29499f220368f8282eda43797ddf8c
Morgen Peschke <morgen.peschke@calltrackingmetrics.com>
2014-04-21 13:49:59 -0400

    Adding rdoc documentation

--- lib/table-name-to-class.rb
+++ lib/table-name-to-class.rb
@@ -1,17 +1,41 @@
 class TableNameToClass
   @@conversion_hash = nil
 
+  ##
+  # Convert a table name into a class constant (or nil if no constant for that
+  # table)
+  # ---
+  # Expects:
+  #   name (object like string):: Name of database table, this can be anything
+  #                               that returns a valid table name when hit with
+  #                               to_s
+  #   force_update (boolean):: Should the table/class correspondence be updated?
+  #                            Any none-nil value triggers a reload.
+  #
+  # Returns:
+  #   nil if the lookup fails, the constant for the class associated with the
+  #   table otherwise.
   def self.convert(name, force_update = nil)
     init_hash unless @@conversion_hash || force_update
     @@conversion_hash[name.to_s]
   end
 
+  ##
+  # Debug helper
+  # ---
+  # Returns the hash of table name and class constant pairs
   def self.debug
     return @@conversion_hash
   end
 
   private
 
+  ## :doc:
+  # Loads the correspondence cache
+  # ----
+  # Recursively visits every .rb file in app/models and requires them all. This
+  # is required to avoid failures when trying to convert the name of a table
+  # who's model has not yet been loaded.
   def self.init_hash
     # Expanded from idea posted here: http://stackoverflow.com/a/6150228
 
